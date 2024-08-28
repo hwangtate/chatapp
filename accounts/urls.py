67b81e3ee3796f6af6e5934708993cf5c8c9d1e9
hwangtate <ghkdxodud10253504@gmail.com>
2024-08-28 16:34:15 +0900

    Fix: django url 규칙으로 url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path("users/", views.user_list, name="user_list"),
-    path("users/<int:pk>", views.user_detail, name="user_detail"),
-    path("profile/", views.user_profile, name="user_profile"),
-    path("register/", views.user_register, name="user_register"),
-    path("login/", views.user_login, name="user_login"),
-    path("logout/", views.user_logout, name="user_logout"),
+    path("users/<int:pk>/", views.user_detail, name="user_detail"),
+    path("user/profile/", views.user_profile, name="user_profile"),
+    path("user/register/", views.user_register, name="user_register"),
+    path("user/login/", views.user_login, name="user_login"),
+    path("user/logout/", views.user_logout, name="user_logout"),
+    # path("change-email/", views.change_email, name="change_email"),
+    # path("change-password/", views.change_password, name="change_password"),
+    # path("find-password/", views.find_password, name="find_password"),
 ]
