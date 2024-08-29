a3ec38767ebfe507aadf8bbf5ecb406d14a67119
hwangtate <ghkdxodud10253504@gmail.com>
2024-08-29 17:04:50 +0900

    Fix: 중복코드 삭제

--- coreapp/settings/development.py
+++ coreapp/settings/development.py
@@ -4,8 +4,6 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 DATABASES = {
