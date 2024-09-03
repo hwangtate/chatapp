6333bd0f6da60f42b384f014a733639fe766955c
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-03 14:29:10 +0900

    Refactor: url account 로 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("user/profile/", views.user_profile, name="user_profile"),
-    path("user/register/", views.user_register, name="user_register"),
-    path("user/login/", views.user_login, name="user_login"),
-    path("user/logout/", views.user_logout, name="user_logout"),
-    path("user/change-email/", views.user_change_email, name="user_change_email"),
-    path("user/reset-password/", views.reset_password, name="reset_password"),
-    path("user/send/change-email/", views.send_change_email_mail, name="send_change"),
-    path("user/send/register/", views.send_register_mail, name="send_register"),
+    path("profile/", views.user_profile, name="user_profile"),
+    path("register/", views.user_register, name="user_register"),
+    path("login/", views.user_login, name="user_login"),
+    path("logout/", views.user_logout, name="user_logout"),
+    path("change-email/", views.user_change_email, name="user_change_email"),
+    path("reset-password/", views.reset_password, name="reset_password"),
+    path("send/change-email/", views.send_change_email_mail, name="send_change"),
+    path("send/register/", views.send_register_mail, name="send_register"),
     path("verify/", views.VerifyEmail.as_view(), name="verify_email"),
     path("active/", views.ActivateUser.as_view(), name="activate_user"),
 ]
