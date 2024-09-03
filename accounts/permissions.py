7f42bdf25e2cae38553459f128a72b8becf3b185
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-03 12:25:49 +0900

    Feat: IsEmailVerified Permission 추가

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,6 +1,11 @@
-# from rest_framework.permissions import BasePermission
-#
-#
+from rest_framework.permissions import BasePermission
+
+
 # class IsAdminUser(BasePermission):
 #     def has_permission(self, request, view):
 #         return request.user and request.user.is_superuser
+
+
+class IsEmailVerified(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.email_is_verified
